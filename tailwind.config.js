cbedef9ad1565dcfc5515418138e9bce98942b99
Wildan132 <mwp113322758@gmail.com>
2022-12-22 23:10:25 +0700

    Upload project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Montserrat': 'Montserrat',
+      },
+
+      colors: {
+        'primary': '#4E5150',
+        'secondary': '#A9A9A9',
+        'box': '#E5E5E5'
+      }
+    },
+  },
+  plugins: [],
+}
